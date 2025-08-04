56788c377ff18a588e97c65319adcd4356a741a0
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-08-04 08:55:56 +0600

    complete the project almost.

--- project 4/Page_View_Time_Series_Visualizer/main.py
+++ project 4/Page_View_Time_Series_Visualizer/main.py
@@ -1,5 +1,5 @@
 import time_series_visualizer as ts
-
+## Drawing the plots using the functions from time_series_visualizer module
 ts.draw_line_plot()
 ts.draw_bar_plot()
 ts.draw_box_plot()
--- project 4/Page_View_Time_Series_Visualizer/test_module.py
+++ project 4/Page_View_Time_Series_Visualizer/test_module.py
@@ -1,35 +1,26 @@
 import unittest
-import pandas as pd
-import seaborn as sns
 import matplotlib
-matplotlib.use('Agg')  # Backend set for testing without GUI
-import matplotlib.pyplot as plt
+matplotlib.use('Agg')  # GUI ছাড়া test চালানোর জন্য উপযুক্ত
 
-from Page_View_Time_Series_Visualizer import draw_cat_plot, draw_heat_map
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+import matplotlib.figure
 
-class DataCleaningTestCase(unittest.TestCase):
-    def test_data_cleaning(self):
-        df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
+class TestTimeSeriesVisualizer(unittest.TestCase):
 
-        # Check that the number of rows is correct after filtering
-        df_clean = df[
-            (df['value'] >= df['value'].quantile(0.025)) &
-            (df['value'] <= df['value'].quantile(0.975))
-        ]
-        self.assertTrue(len(df_clean) > 1200, "Cleaned data should have more than 1200 rows")
+    def test_line_plot(self):
+        fig = draw_line_plot()
+        self.assertIsInstance(fig, matplotlib.figure.Figure)
+        fig.savefig("test_line_plot_output.png")
 
-class PlotTestCase(unittest.TestCase):
-    def test_cat_plot(self):
-        fig = draw_cat_plot()
-        self.assertIsNotNone(fig, "draw_cat_plot should return a figure.")
-        self.assertIsInstance(fig, matplotlib.figure.Figure, "Output should be a matplotlib figure.")
-        fig.savefig("test_cat_plot_output.png")  # Optional: save test output
+    def test_bar_plot(self):
+        fig = draw_bar_plot()
+        self.assertIsInstance(fig, matplotlib.figure.Figure)
+        fig.savefig("test_bar_plot_output.png")
 
-    def test_heat_map(self):
-        fig = draw_heat_map()
-        self.assertIsNotNone(fig, "draw_heat_map should return a figure.")
-        self.assertIsInstance(fig, matplotlib.figure.Figure, "Output should be a matplotlib figure.")
-        fig.savefig("test_heat_map_output.png")  # Optional: save test output
+    def test_box_plot(self):
+        fig = draw_box_plot()
+        self.assertIsInstance(fig, matplotlib.figure.Figure)
+        fig.savefig("test_box_plot_output.png")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
